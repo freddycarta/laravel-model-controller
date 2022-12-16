154e7102e113eb355459574bb242be34771394ce
freddycarta <cartafederico92@gmail.com>
2022-12-16 16:38:08 +0100

    route

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        $movies = ['title' => 'title 1']; 
+        return view('home', compact('movies'))
+    }
 }
